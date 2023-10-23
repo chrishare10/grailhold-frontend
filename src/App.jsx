1c2727a449ab5515308c0dbbdf1af9636199f4bd
chrishare10 <chrishare.png@gmail.com>
2023-10-22 21:42:49 -0400

    cycleRaycast addition

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 
-import Map from "./components/Map";
 import { useRef, useEffect, useState } from "react";
 import NavButton from "./components/NavButton";
 import Nav from "./components/Nav";
 import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
-import { useSpring, animated } from '@react-spring/web'
 import { pinchAction, createUseGesture } from '@use-gesture/react'
 import AboutWrapper from "./components/AboutWrapper";
 import Panels from "./components/Panels";
@@ -13,10 +11,7 @@ import FullCanvas from "./components/FullCanvas";
 const useGesture = createUseGesture([pinchAction])
 
 export default function App(){
-    const pageWrapper = useRef()
-    const mapWrapper = useRef()
-    const mapContainer = useRef()
-    const wheelOffset = useRef(1)
+
 
     const [date, setDate] = useState(new Date());
     const [oceanColor, setOceanColor] = useState('bg-blue-200');
@@ -41,65 +36,13 @@ export default function App(){
             setOceanColor('bg-blue-700')
         }
 
-        console.log(oceanColor)
+      
 
     }, [currentHour])
 
     
 
-    // useEffect(() => {
-    //     const handler = (e) => e.preventDefault()
-    //     document.addEventListener('gesturestart', handler)
-    //     document.addEventListener('gesturechange', handler)
-    //     document.addEventListener('gestureend', handler)
-    //     return () => {
-    //       document.removeEventListener('gesturestart', handler)
-    //       document.removeEventListener('gesturechange', handler)
-    //       document.removeEventListener('gestureend', handler)
-    //     }
-    //   }, [])
    
-    // // let mapScale
-    // // let scalePercent
-    // // let panning = false
-    // // let scale = 1
-    // // let pointX = 0
-    // // let pointY = 0
-    // // let start = { x: 0, y: 0 }
-    // // const speed = 0.01;
-
-    // const [style, api] = useSpring(() => ({
-    //     x: 0,
-    //     y: 0,
-    //     scale: 1,
-    //     rotateZ: 0,
-    //   }))
-
-
-    // useGesture(
-    //     { 
-    //         onPinch:  ({ origin: [ox, oy], first, movement: [ms], offset: [s, a], memo }) => {
-    //             if (first) {
-    //             const { width, height, x, y } = mapWrapper.current.getBoundingClientRect()
-    //             const tx = ox - (x + width / 2)
-    //             const ty = oy - (y + height / 2)
-    //             memo = [style.x.get(), style.y.get(), tx, ty]
-    //             }
-        
-    //             const x = memo[0] - (ms - 1) * memo[2]
-    //             const y = memo[1] - (ms - 1) * memo[3]
-    //             api.start({ scale: s, rotateZ: a, x, y })
-    //             return memo
-    //         }
-    //     },
-    //     {
-    //         target: mapWrapper,
-    //         eventOptions: { passive: false },
-    //         pinch: { scaleBounds: { min: 0.5, max: 2 }, rubberband: true }
-    //     }
-    // )
-
- 
 
     
 
