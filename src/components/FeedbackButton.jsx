30a4ec2f98695a05567918275669a188ac69113a
chrishare10 <chrishare.png@gmail.com>
2023-11-22 23:17:30 -0500

    moved feedback btn slightly

--- src/components/FeedbackButton.jsx
+++ src/components/FeedbackButton.jsx
@@ -2,5 +2,5 @@
 
 export default function FeedbackButton() {
 
-    return <a href="https://docs.google.com/forms/d/e/1FAIpQLSdueyUSoaHG1KVQ5NZ6_TWfHljvgqrO3UlFAMJYfk2bYrMIgQ/viewform?usp=sf_link" className="absolute left-10 top-10 z-10 py-2 px-3 bg-gColorOne hover:bg-gColorTwo rounded-md text-white" target="_blank">Leave Feedback</a>
+    return <a href="https://docs.google.com/forms/d/e/1FAIpQLSdueyUSoaHG1KVQ5NZ6_TWfHljvgqrO3UlFAMJYfk2bYrMIgQ/viewform?usp=sf_link" className="absolute left-5 top-5 z-10 py-2 px-3 bg-gColorOne hover:bg-gColorTwo rounded-md text-white" target="_blank">Leave Feedback</a>
 }
